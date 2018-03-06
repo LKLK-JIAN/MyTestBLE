bee8b4ae0d03e5d8608c225f281a345c53664d1e
liuyvhao <yvhao_liu@163.com>
2018-03-06 15:00:45 +0800

    添加system

--- app/src/main/java/illuminometer/com/example/android/mytestble/BatteryActivit.java
+++ app/src/main/java/illuminometer/com/example/android/mytestble/BatteryActivit.java
@@ -50,6 +50,8 @@ public class BatteryActivit extends AppCompatActivity {
             int current=intent.getExtras().getInt("level");//获得当前电量
             int total=intent.getExtras().getInt("scale");//获得总电量
             int percent=current*100/total;
+            System.out.print("0");
+            //batterytv.setText("现在的电量是"+percent+"%。");
             batterytv.setText("现在的电量是"+percent+"%。");
         }
     }
